d4f797c3c1be935c9a26bd05eeb3c8267fd83fd4
Echezonac <hampoherobede@gmail.com>
2022-02-04 11:14:55 +0100

    error 505 debugging

--- HRL/settings.py
+++ HRL/settings.py
@@ -127,13 +127,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
